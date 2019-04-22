30b052f8e02bc336eff55f4bf010d1ba03bb62e3
Nisshii4124 <nkc.daiki.nishiwaki@gmail.com>
2019-04-22 14:54:28 +0900

    AudioManagerできた

--- Assets/Nishiwaki/Script/TestMusic.cs
+++ Assets/Nishiwaki/Script/TestMusic.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingletonMonoBehaviour : MonoBehaviour
+public class TestMusic : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.Instance.PlayBGM(AUDIO.BGM_BGM_MAOUDAMASHII_FANTASY13, AudioManager.BGM_FADE_SPEED_RATE_HIGH);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }

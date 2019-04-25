4e3616dc4d34e62261f5c15d4faa3d14c2ac2fd2
Nisshii4124 <nkc.daiki.nishiwaki@gmail.com>
2019-04-25 10:15:32 +0900

    弾が追尾するようになった

---
+++ Assets/Nishiwaki/Script/Tracking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracking : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+    public Transform target;
+    void Update()
+    {
+        //GameObject player = GameObject.Find("Player");
+        //Transform target = Transform.Find("Player");
+        float step = Time.deltaTime * speed;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        transform.LookAt(target);
+    }
+}

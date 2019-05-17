632b326991ef34d0413a121c8b55f07c7061c018
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2019-05-17 14:01:20 +0900

    隕石落ちるかも

--- Assets/mase/MeteorGenerator.cs
+++ Assets/mase/MeteorGenerator.cs
@@ -10,13 +10,22 @@ public class MeteorGenerator : MonoBehaviour
 
     void Start()
     {
-        InvokeRepeating("Meteorpos", 1, 1);
+        //InvokeRepeating("Meteorpos", 1, 1);
+    }
+
+     void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(MeteorPrefab, new Vector3(-2.5f + 5 * Random.value, 9, 0), Quaternion.identity);
+            Debug.Log("でた");
+        }
     }
 
     // Update is called once per frame
-    void Meteorpos()
-    {
-        Instantiate(MeteorPrefab, new Vector3(-2.5f + 5 * Random.value, 9, 0), Quaternion.identity);
-        Debug.Log("でた");
-    }
+    //void Meteorpos()
+    //{
+    //    Instantiate(MeteorPrefab, new Vector3(-2.5f + 5 * Random.value, 9, 0), Quaternion.identity);
+    //    Debug.Log("でた");
+    //}
 }

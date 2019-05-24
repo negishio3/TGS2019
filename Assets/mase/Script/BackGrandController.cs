24f4ec1548151350cb33676e1782a24dac58b2ba
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2019-05-24 10:30:44 +0900

    背景スクロールの追加

---
+++ Assets/mase/Script/BackGrandController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGrandController : MonoBehaviour
+{
+    float BackGrand_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackGrand_pos = this.gameObject.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-0.1f, 0, 0);
+        if (transform.position.x < -10f)
+        {
+            transform.position = new Vector3(10f, BackGrand_pos, 0);
+        }
+    }
+}

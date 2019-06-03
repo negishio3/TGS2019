9a9ea54bb1aed09153f3b2670c34f8f6826e2816
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2019-06-03 13:34:15 +0900

    プッシュ

--- Assets/mase/Script/UFOGenerator.cs
+++ Assets/mase/Script/UFOGenerator.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class UFOGenerator : MonoBehaviour
 {
-    public GameObject CreatePos_Left;//生成場所（左）
-    public GameObject CreatePos_Right;//生成場所（右）
+    //public GameObject CreatePos_Left;//生成場所（左）
+    //public GameObject CreatePos_Right;//生成場所（右）
+    //public GameObject UFOobj_Left;
+    //public GameObject UFOobj_Right;
     float CreateTime = 15;//生成間隔
+    public GameObject UfO_obj;
     GameObject Pos;
-
+    int POS = 1;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,11 +26,20 @@ public class UFOGenerator : MonoBehaviour
         CreateTime -= Time.deltaTime;
         if (CreateTime <= 0f)
         {
-             Pos = Random.Range();
-            if ()
-            {
-
-            }
+            CreateTime = 15;
+            //int Pos = Random.Range(0,2);
+            Instantiate(UfO_obj, new Vector3(-5f, 5f, 0f), Quaternion.identity);
+            //Debug.Log(Pos);
+            //Debug.Log(CreateTime);
+            //if (Pos <= 0)
+            //{
+            //    Instantiate(UFOobj_Left, new Vector3(5f, 5f, 0f), Quaternion.identity);
+            //}
+            //if (Pos >=1)
+            //{
+            //    Instantiate(UFOobj_Right,new Vector3(-5f,5f,0f), Quaternion.identity);
+            //    Debug.Log("北");
+            //}
         }
     }
 }
--- Assets/mase/Script/UFO_Left.cs
+++ Assets/mase/Script/UFO_Left.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UFO : MonoBehaviour,i_Objects
+public class UFO_Left : MonoBehaviour, i_Objects
 {
     public float movespeed;//UFOの移動速度
     public int HP_UFO;//UFOのHP
-    float UFO_rotspeed=10;
+    float UFO_rotspeed = 10;
     float SwingRange = 15;
     Vector3 rot_UFO;
     float UFO_pos;
 
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         //UFOのオブジェクトの位置情報を代入
@@ -21,13 +21,13 @@ public class UFO : MonoBehaviour,i_Objects
     // Update is called once per frame
     void Update()
     {
-        //右に移動
-        transform.Translate(movespeed, -0.01f, 0, Space.World);
-        rot_UFO = new Vector3(Mathf.Sin(Time.time*UFO_rotspeed)*SwingRange,UFO_rotspeed*Time.time,0);
+        //hidariに移動
+        transform.Translate(movespeed, +0.01f, 0, Space.World);
+        rot_UFO = new Vector3(Mathf.Sin(Time.time * UFO_rotspeed) * SwingRange, UFO_rotspeed * Time.time, 0);
         transform.eulerAngles = rot_UFO;
         //上下移動
-        transform.position = new Vector3(transform.position.x, UFO_pos+Mathf.PingPong(Time.time, 1), transform.position.z);
-        if (HP_UFO<=0)
+        transform.position = new Vector3(transform.position.x, UFO_pos + Mathf.PingPong(Time.time, 1), transform.position.z);
+        if (HP_UFO <= 0)
         {
             //Instantiate(Item,transform.position, Quaternion.identity);//生成する
             Destroy(gameObject);//㏋が0になったら消す

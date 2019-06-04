d5b09501163d0fbd6beeb6ecc4ee387cb83af6c4
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2019-06-04 14:51:59 +0900

    UFO左右生成可能になった

--- Assets/mase/Script/UFOGenerator.cs
+++ Assets/mase/Script/UFOGenerator.cs
@@ -6,10 +6,10 @@ public class UFOGenerator : MonoBehaviour
 {
     //public GameObject CreatePos_Left;//生成場所（左）
     //public GameObject CreatePos_Right;//生成場所（右）
-    //public GameObject UFOobj_Left;
-    //public GameObject UFOobj_Right;
+    public GameObject UFOobj_Left;
+    public GameObject UFOobj_Right;
     float CreateTime = 15;//生成間隔
-    public GameObject UfO_obj;
+    //public GameObject UfO_obj;
     GameObject Pos;
     int POS = 1;
 
@@ -27,21 +27,21 @@ public class UFOGenerator : MonoBehaviour
         if (CreateTime <= 0f)
         {
             CreateTime = 15;
-            //int Pos = Random.Range(0,2);
-            Instantiate(UfO_obj, new Vector3(-5f, 5f, 0f), Quaternion.identity);
+            int Pos = Random.Range(0, 2);
+            //Instantiate(UfO_obj, new Vector3(-5f, 5f, 0f), Quaternion.identity);
             MeteorGenerator.Instance.ChangeUFOFlg(true);
 
             //Debug.Log(Pos);
             //Debug.Log(CreateTime);
-            //if (Pos <= 0)
-            //{
-            //    Instantiate(UFOobj_Left, new Vector3(5f, 5f, 0f), Quaternion.identity);
-            //}
-            //if (Pos >=1)
-            //{
-            //    Instantiate(UFOobj_Right,new Vector3(-5f,5f,0f), Quaternion.identity);
-            //    Debug.Log("北");
-            //}
+            if (Pos <= 0)
+            {
+                Instantiate(UFOobj_Left, new Vector3(5f, 5f, 0f), Quaternion.identity);
+            }
+            if (Pos >= 1)
+            {
+                Instantiate(UFOobj_Right, new Vector3(-5f, 5f, 0f), Quaternion.identity);
+                Debug.Log("北");
+            }
         }
     }
 

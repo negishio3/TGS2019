daff3742f900b81e94fc8359dd3185dd63bc97cf
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2019-06-17 14:26:26 +0900

    雲反対に向かっていく

---
+++ Assets/mase/Script/CloudGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloudGenerator : MonoBehaviour
+{
+    //public GameObject[] Cloud;
+    public GameObject obj;
+    float CreateTime = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int Clouds = Random.Range(0, Cloud.Length);
+        // Instantiate(Cloud[Clouds],)
+        CreateTime -= Time.deltaTime;
+        if (CreateTime<=0f)
+        {
+            Instantiate(obj, this.transform.position, Quaternion.identity);
+            Debug.Log("生成");
+            CreateTime = 1;
+        }
+    }
+}
---
+++ Assets/mase/Script/CloudMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloudMove : MonoBehaviour
+{
+    public float MoveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(MoveSpeed, -0.01f,0, Space.World);
+
+        if ()
+        {
+
+        }
+    }
+}

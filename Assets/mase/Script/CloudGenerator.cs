e4a3305405a9eac6b386b8b6c0c8991ee3953630
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2019-10-03 10:18:19 +0900

    雲

--- Assets/mase/Script/CloudGenerator.cs
+++ Assets/mase/Script/CloudGenerator.cs
@@ -8,12 +8,16 @@ public class CloudGenerator : MonoBehaviour
     public GameObject[] Cloud; //雲の保管場所
     //public GameObject obj;
     float CreateTime = 1.0f;
+    float[] cloudSize = new float [3];
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < Cloud.Length; i++)
+        {
+            cloudSize[i] = Cloud[i].GetComponent<SpriteRenderer>().bounds.size.x;
+        }
     }
 
     // Update is called once per frame
@@ -25,7 +29,7 @@ public class CloudGenerator : MonoBehaviour
         if (CreateTime<=0f)
         {
             int Clouds = Random.Range(0, Cloud.Length);
-            Instantiate(Cloud[Clouds], new Vector3(5, Random.Range(7f, 9f), -1), Quaternion.identity);
+            Instantiate(Cloud[Clouds], new Vector3(Camera.main.ViewportToWorldPoint(Vector3.one).x+cloudSize[Clouds]/2, Random.Range(7f, 9f), -1), Quaternion.identity);
             CreateTime = 3;
         }
     }
--- Assets/mase/Script/CloudMove.cs
+++ Assets/mase/Script/CloudMove.cs
@@ -7,24 +7,28 @@ public class CloudMove : MonoBehaviour
 {
     //float MoveSpeed;
     float rnd;
-    float LifeTime;
-    public float Speed;
+    float lifeTime;
+    float speed = 0.05f;
+
+    SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        speed = speed / sr.bounds.size.x;
         rnd = Random.value;
         //MoveSpeed = 0.015f + 0.03f * rnd;
-        LifeTime = 10;
+        lifeTime = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.Translate(-MoveSpeed, 0,0, Space.World);
-        transform.Translate(-Speed, 0, 0, Space.World);
-        LifeTime -= Time.deltaTime;
-        if (LifeTime<=0)
+        transform.Translate(-speed, 0, 0, Space.World);
+        lifeTime -= Time.deltaTime;
+        if (transform.position.x <= Camera.main.ViewportToWorldPoint(Vector3.zero).x - sr.bounds.size.x/2.0f)
         {
             Destroy(gameObject);
         }
